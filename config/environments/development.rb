18562a1d028b1eb02000ec6b4ae8da25ca293352
Mayank Kashyap <mayankkashyap95@gmail.com>
2017-07-17 09:02:42 +0530

    mailgun

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -32,12 +32,21 @@ Rails.application.configure do
   config.action_mailer.perform_caching = false
 
   # Change mail delvery to either :smtp, :sendmail, :file, :test
-  config.action_mailer.delivery_method = :smtp
-  config.action_mailer.smtp_settings = {
-    address: "smtp.gmail.com",
-    port: 465,
-    user_name: "resumebuilderbymk@gmail.com",
-    password: "re1su2me3"
+  # config.action_mailer.delivery_method = :smtp
+  # config.action_mailer.perform_deliveries = true
+  # config.action_mailer.smtp_settings = {
+  #     :address              => "smtp.mailgun.org",
+  #     :port                 => 578,
+  #     :domain               => "gmail.com",
+  #     :user_name            => "postmaster@resumebuilder.mayankkashyap.com",
+  #     :password             => "re1su2me3",
+  #     :authentication       => :login,
+  # }
+
+  config.action_mailer.delivery_method = :mailgun
+  config.action_mailer.mailgun_settings = {
+    api_key: 'api-key-e74f7c4904d4e23c38a334e4e940824b',
+    domain: 'resumebuilder.mayankkashyap.com',
   }
 
   # Specify what domain to use for mailer URLs
