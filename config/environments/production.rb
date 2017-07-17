0f955d0f8128aed6ac182792a90c33026e3229b4
root <Serveratdigitalocean>
2017-07-17 00:46:57 -0400

    mailer setup

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -57,10 +57,21 @@ Rails.application.configure do
   # config.active_job.queue_name_prefix = "resume_#{Rails.env}"
   config.action_mailer.perform_caching = false
 
-  config.action_mailer.delivery_method = :mailgun
-  config.action_mailer.mailgun_settings = {
-    api_key: 'api-key-e74f7c4904d4e23c38a334e4e940824b',
-    domain: 'resumebuilder.mayankkashyap.com',
+  config.action_mailer.delivery_method = :smtp
+#  config.action_mailer.mailgun_settings = {
+ #   api_key: 'key-e74f7c4904d4e23c38a334e4e940824b',
+  #  domain: 'resumebuilder.mayankkashyap.com',
+ # }
+
+config.action_mailer.smtp_settings = {
+    address: "smtp.mayankkashyap.com",
+    port: 587,
+    domain: "resumebuilder.mayankkashyap.com",
+    authentication: "plain",
+    enable_starttls_auto: true,
+    user_name: "resumebuilder@mayankkashyap.com",
+    password: "xo$Rz@V4",
+    openssl_verify_mode: 'none'
   }
 
   # config.action_mailer.smtp_settings = {
