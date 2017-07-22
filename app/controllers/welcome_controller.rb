172ec8919e80e96521adadb35069d01c49916a81
Mayank Kashyap <mayankkashyap95@gmail.com>
2017-07-22 16:16:21 +0530

    Login made optional

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -7,26 +7,26 @@ class WelcomeController < ApplicationController
   end
 
   def template1
-    if session[:user_id]
+    # if session[:user_id]
       render :layout => "template1"
-    else    
-      redirect_to '/'
-    end 
+    # else    
+      # redirect_to '/'
+    # end 
   end
 
   def template2
-    if session[:user_id]
+    # if session[:user_id]
       render :layout => "template2"
-    else    
-      redirect_to '/'
-    end 
+    # else    
+      # redirect_to '/'
+    # end 
   end
 
   def template3
-    if session[:user_id]
+    # if session[:user_id]
       render :layout => "template3"
-    else    
-      redirect_to '/'
-    end 
+    # else    
+      # redirect_to '/'
+    # end 
   end
 end
